0c7ff9b033bb6d5b1cb0934c6a0a8ddd0685f20e
Silvia Trejo <s.y.trejo26@gmail.com>
2024-06-26 21:43:08 -0400

    6.25 problem 1

---
+++ StarsFromString.java
@@ -0,0 +1,9 @@
+class StarsFromString {
+    public static void main(String[] args) {
+        String s = "Hello, World!";
+        for (int i = 0; i < s.length(); i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
