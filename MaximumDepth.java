d3c3568603b7bb397fb2a525b24228dc400c163a
Silvia Trejo <s.y.trejo26@gmail.com>
2024-08-14 14:08:32 -0400

    Aug 14 problem

---
+++ MaximumDepth.java
@@ -0,0 +1,12 @@
+import javax.swing.tree.TreeNode;
+
+class MaximumDepth {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+}
